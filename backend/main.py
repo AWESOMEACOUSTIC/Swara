237962aedadb627edb39d410d38393e6587c5c79
Aditya <adityapanigrahy98@gmail.com>
2026-04-24 15:36:21 +0530

    feat(backend): initialize Modal music generation service
    Add ACE-Step pipeline, Qwen2 LLM integration, GPU config, volumes, and model lifecycle setup.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
-import os
-
+import base64
+from typing import List
+import uuid
 import modal
+import os
+import boto3
 
 app = modal.App("swara")
 image = (
@@ -18,10 +21,40 @@ hf_volume = modal.Volume.from_name("qwen-hf-cache", create_if_missing=True)
 
 swara_gen_secrets = modal.Secret.from_name("swara-gen-secret")
 
-@app.function(image = image, gpu = "Nvidia L4", secrets=[modal.Secret.from_name("swara-gen-secret")])
-def function_test():
-    print("Hello from Modal!")
-    print(os.environ["test"])
+@app.cls(
+    image=image,
+    gpu = "Nvidia L4",
+    volumes={"/models": modal_volume, "/.cache/huggingface": hf_volume},
+    secret=[swara_gen_secrets],
+    scaledown_window=15,
+)
+
+class MusicGenServer:
+    @modal.enter()
+    def load_model(self):
+        from acestep.pipeline_ace_step import ACEStepPipeline
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+
+        # Music Generation Model
+        self.music_model = ACEStepPipeline(
+            checkpoint_dir =  "/models",
+            dtype="bfloat16",
+            torch_compile=False,
+            cpu_offload=False,
+            overlapped_decode=False
+        )
+
+        # Large Language Model
+        model_id = "Qwen/Qwen2-7B-Instruct"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.llm_model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype="auto",
+            device_map="auto",
+            cache_dir="/.cache/huggingface"
+        )
+
+    
 
 
 @app.local_entrypoint()
